092367ba678b4eb3dc8e2e4ec4071cbb065f4305
flutter <sazzadhossain5213@gmail.com>
2023-11-03 00:34:56 +0600

    update

---
+++ lib/homepage.dart
@@ -0,0 +1,77 @@
+
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ostad/screen/apihit.dart';
+import 'package:http/http.dart' as http;
+import 'package:ostad/secondpage.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  List<Apihit>adduser=[];
+  Future<List<Apihit>>getApi()async{
+    final responce =await http.get(Uri.parse("https://jsonplaceholder.typicode.com/photos"));
+    var data=jsonDecode(responce.body.toString());
+    if(responce.statusCode==200){
+      for(Map<String ,dynamic>i in data){
+        adduser.add(Apihit.fromJson(i));
+      }
+      return adduser;
+    }
+    else{
+      return adduser;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Photo Gallary Apps"),),
+body: FutureBuilder(future: getApi(),
+    builder: (context,snapshot){
+  if(!snapshot.hasData){
+    return Center(child: CupertinoActivityIndicator());
+  }
+  else{
+return ListView.builder(
+
+        itemCount: adduser.length,
+        itemBuilder: (context,index){
+      return GestureDetector(
+        onTap: (){
+   Navigator.push(context, MaterialPageRoute(builder: (context){
+     return Secondpage(adduser[index]);
+   }));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 100,
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Image(image: NetworkImage(adduser[index].url.toString()))),
+                SizedBox(width: 20,),
+                Expanded(
+                    flex:2,
+                    child: Text(adduser[index].title.toString(),style: TextStyle(fontWeight: FontWeight.bold,),)),
+
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+    }),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ostad/homepage.dart';
+
+void main(){
+  runApp(Myapps());
+}
+class Myapps extends StatelessWidget {
+  const Myapps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
+
+
+
